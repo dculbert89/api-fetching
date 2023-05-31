8c91cb431daa75a184a7faf487a8a31b980dd122
dculbert89 <dculbert89@gmail.com>
2023-05-30 22:43:43 -0400

    api-fetching post section fixed

--- index.js
+++ index.js
@@ -24,30 +24,12 @@ async function fetchProductsData () {
 async function renderProductContainer() {
     let container = document.getElementById("products-container")
 
-    let productsInContainer = await fetchProductsData();
-    // console.log(productsInContainer)
+    let productInContainer = await fetchProductsData();
+    // console.log(productInContainer)
     
-    for(i = 0; i < productsInContainer.length; i++){
-        let myCurrentProductInContainer = productsInContainer[i];
-
-        let newProductInContainer = document.createElement("p");
-        newProductInContainer.classList.add("productItems")
-
-        newProductInContainer.innerText = myCurrentProductInContainer.title
-
-        container.appendChild(newProductInContainer);
-    }
-}
-
-async function renderPostContainer() {
-    let container = document.getElementById("posts-container")
-
-    let postInContainer = await fetchProductsData();
-    console.log(postInContainer)
-    
-    for(i = 0; i < postInContainer.length; i++){
-        let myCurrentPostContainer = postInContainer[i];
-        // console.log(myCurrentPostContainer)
+    for(i = 0; i < productInContainer.length; i++){
+        let myCurrentProduct = productInContainer[i];
+        // console.log(myCurrentProduct)
 
         let productTitle = document.createElement("h3");
         let productDescription = document.createElement("p");
@@ -55,19 +37,59 @@ async function renderPostContainer() {
         let productDiscountPercentage = document.createElement("p");
         let productImage = document.createElement("img");
 
-        productTitle.innerText = myCurrentPostContainer.title;
-        productDescription.innerText = `Description: ${myCurrentPostContainer.description}`;
-        productPrice.innerText = `Price: $${myCurrentPostContainer.price}`;
-        productDiscountPercentage.innerText = `Discount: $${myCurrentPostContainer.discountPercentage}`;
-        productImage.src = myCurrentPostContainer.images[0]
-
+        productTitle.innerText = myCurrentProduct.title;
+        productDescription.innerText = `Description: ${myCurrentProduct.description}`;
+        productPrice.innerText = `Price: $${myCurrentProduct.price}`;
+        productDiscountPercentage.innerText = `Discount: $${myCurrentProduct.discountPercentage}`;
+        productImage.src = myCurrentProduct.images[0]
         
         container.appendChild(productTitle);
         container.appendChild(productImage)
         container.appendChild(productDescription);
         container.appendChild(productPrice);
         container.appendChild(productDiscountPercentage);
+    }
+}
+    
+async function fetchPostData () {
+    try {
 
+        let response = await fetch(`${Base_URL}/posts`);
+        // console.log(response);
+
+        let translatedData = await response.json();
+        // console.log(translatedData)
+
+        let actualProductData = translatedData.posts
+        // console.log(actualProductData)
+
+        return actualProductData
+
+    }
+    catch(error) {
+        console.log(error);
+
+    }
+};
+
+async function renderPostContainer() {
+    let container = document.getElementById("posts-container")
+
+    let postInContainer = await fetchPostData();
+    // console.log(postInContainer)
+    
+    for(i = 0; i < postInContainer.length; i++){
+        let myCurrentPostContainer = postInContainer[i];
+        // console.log(myCurrentPostContainer)
+
+        let postTitle = document.createElement("h3");
+        let postBody = document.createElement("p");
+
+        postTitle.innerText = myCurrentPostContainer.title;
+        postBody.innerText = myCurrentPostContainer.body;
+           
+        container.appendChild(postTitle);
+        container.appendChild(postBody)
     }
 }
 
